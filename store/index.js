3835f8c4c081fcaacd8f288f080ce5551c317de0
Stanislav Karassyov <stanislav.krsv@gmail.com>
2017-10-14 19:52:10 +0600

    feat: Added AsyncStorage synchronization

--- store/index.js
+++ store/index.js
@@ -1,11 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import { AsyncStorage } from 'react-native'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
 
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  undefined,
+  compose(applyMiddleware(thunk), autoRehydrate())
 )
+persistStore(store, { storage: AsyncStorage })
 
 export default store
